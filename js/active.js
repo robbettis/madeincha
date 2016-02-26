48f2f9010962146c74ecf23c43fb8e184d68c4fb
Rob Bettis <rob@bett.is>
2016-02-26 08:56:45 -0500

    Finetune Zoom
    from 17 to 10

--- js/active.js
+++ js/active.js
@@ -196,7 +196,7 @@ function init_masonry(){
 function gmap_init() {
 var mapOptions = {
   scrollwheel: false,
-zoom: 18,
+zoom: 10,
 center: new google.maps.LatLng(35.0485769, -85.3103398), // Chattanooga
 styles: [{featureType:"landscape",stylers:[{saturation:-100},{lightness:65},{visibility:"on"}]},{featureType:"poi",stylers:[{saturation:-100},{lightness:51},{visibility:"simplified"}]},{featureType:"road.highway",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{saturation:-100},{lightness:30},{visibility:"on"}]},{featureType:"road.local",stylers:[{saturation:-100},{lightness:40},{visibility:"on"}]},{featureType:"transit",stylers:[{saturation:-100},{visibility:"simplified"}]},{featureType:"administrative.province",stylers:[{visibility:"off"}]/**/},{featureType:"administrative.locality",stylers:[{visibility:"off"}]},{featureType:"administrative.neighborhood",stylers:[{visibility:"on"}]/**/},{featureType:"water",elementType:"labels",stylers:[{visibility:"on"},{lightness:-25},{saturation:-100}]},{featureType:"water",elementType:"geometry",stylers:[{hue:"#ffff00"},{lightness:-25},{saturation:-97}]}]
 };
